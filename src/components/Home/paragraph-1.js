55e2681ac6891cb0b1a642f5e68799c436f05f78
emontes <emontes@turista.com.mx>
2022-07-16 21:50:31 -0500

    changes to paragraph-1.js

--- src/components/Home/paragraph-1.js
+++ src/components/Home/paragraph-1.js
@@ -4,72 +4,87 @@ import styled from 'styled-components'
 const Paragraph = () => {
   return (
     <Wrapper className="section">
-    <p>
-    <b>2034:</b> Natural disasters and nuclear war made Earth uninhabitable.
-    </p>
-    
-    <p>
-    Citizens of all nations had no choice but to escape to the Metaverse to seek better lives.
-    </p>
-    
-    <p>
-    At first, life returned to normal: a thriving economy and peace amongst the masses.
-    </p>
-    
-    <p>However, things began to change. The same mistakes that doomed the physical world repeated themselves in the digital world.</p>
-    
-<p>A totalitarian regime took control and imposed draconian restrictions, enslaving the masses.</p>
-
-    <p>But all was not lost. A small but fearsome coalition of leaders from business, 
-    technology, and military forged an enterprise in retaliation against the oppression.</p>
-
-    <h3>Novum Labs was born.</h3>
-    
-    <p>It would provide resources to arm the resistance and give them the tools they needed to defend themselves against these tyrants.</p>
-    
-<p>Novum created a decentralized system of factories, laboratories, and training facilities that produced digital commodities and new forms of technology that gave citizens the equipment and intelligence needed to survive.</p>
-    
-<p>Hidden in the shadows, the leadership’s identities and whereabouts are known to no one. But that hasn’t stopped the warlords from tirelessly hunting them for decades.</p>
-<p>Despite mortal threats, they continue to distribute these resources through a network of elite fighters and revolutionaries throughout the world.</p>
-
-
-    
       <p>
-        Once his eyes had grown used to the sight, Wang could see that the faint
-        red background was indeed pulsing. The entire sky flickered, as if the
-        universe was but a quivering lamp in the wind.
+        <b>2034:</b> Natural disasters and nuclear war made Earth uninhabitable.
       </p>
       <p>
-        Standing under the flashing dome of the night sky, Wang suddenly felt
-        the universe shrink until it was so small that only he was imprisoned in
-        it. The universe was a cramped heart, and the red light that suffused
-        everything was the translucent blood that filled the organ. Suspended in
-        the blood, he saw that the flickering of the red light was not
-        periodic—the pulsing was irregular. He felt a strange, perverse, immense
-        presence that could never be understood by human intellect.
+        Citizens of all nations had no choice but to escape to the Metaverse to
+        seek better lives.
       </p>
-
+      <p>
+        At first, life returned to normal: a thriving economy and peace amongst
+        the masses.
+      </p>
+      <p>
+        However, things began to change. The same mistakes that doomed the
+        physical world repeated themselves in the digital world.
+      </p>
+      <p>
+        A totalitarian regime took control and imposed draconian restrictions,
+        enslaving the masses.
+      </p>
+      <p>
+        But all was not lost. A small but fearsome coalition of leaders from
+        business, technology, and military forged an enterprise in retaliation
+        against the oppression.
+      </p>
+      <h3>Novum Labs was born.</h3>
+      <p>
+        It would provide resources to arm the resistance and give them the tools
+        they needed to defend themselves against these tyrants.
+      </p>
+      <p>
+        Novum created a decentralized system of factories, laboratories, and
+        training facilities that produced digital commodities and new forms of
+        technology that gave citizens the equipment and intelligence needed to
+        survive.
+      </p>
+      <p>
+        Hidden in the shadows, the leadership’s identities and whereabouts are
+        known to no one. But that hasn’t stopped the warlords from tirelessly
+        hunting them for decades.
+      </p>
+      <p>
+        Despite mortal threats, they continue to distribute these resources
+        through a network of elite fighters and revolutionaries throughout the
+        world.
+      </p>
       <h2>
         Genesis Collection:{' '}
         <span className="subtitle">
           Kingpin Compound.<span className="white-shape">--</span>
         </span>
       </h2>
-
       <p>
-        Another sixty thousand Trisolaran hours went by. Twenty thousand
-        Trisolaran hours after the completion of the huge particle accelerator
-        in space, the unfolding of the proton into two dimensions was about to
-        begin in a synchronous orbit around Trisolaris.
+        The first offering in the NL collection was originally designed as a
+        hideout for X75.
+      </p>
+      <p>X75 is the most lethal asset in the Novum Labs (NL) network.</p>
+      <p>
+        Loud-mouthed, insubordinate, and draped in audacious attire: his
+        presence was that of a has-been, stripped bare of any dignity and pride.
+        Then, a series of events and a trigger would awaken him, unearthing the
+        god within.
       </p>
       <p>
-        It was a beautiful and mild Stable Era day. The sky was particularly
-        clear.Like the day when the fleet had set sail eighty thousand
-        Trisolaran hours ago, the entire population of Trisolaris looked up into
-        the sky, gazing at that giant ring. The princeps and all the consuls
-        again came and stood under the Pendulum Monument. The pendulum had long
-        stopped, and the weight hung still like a solid rock between the tall
-        pillars. Looking at it, it was hard to believe that it had once moved.
+        Due to the bounty on his head, he was provided with a state-of-the-art
+        facility for safety and a place of respite while he awaited orders for
+        his next mission.
+      </p>
+
+      <p>
+        Armed with weapons, vehicles, and his trusted guard dog CZ, he’s well
+        equipped for the chaos thrown his way.{' '}
+      </p>
+
+      <p>
+        Each one of his most important assets will be available in the genesis
+        collection and is compatible with all Loot Pods in the CloneX ecosystem.{' '}
+      </p>
+
+      <p>
+        They’re the perfect set of accessories for all the other outlaws in the
+        Metaverse looking to fortify their digital dwellings.
       </p>
     </Wrapper>
   )
@@ -80,11 +95,17 @@ export default Paragraph
 const Wrapper = styled.div`
   h2 {
     text-align: center;
-    margin: 15rem 0 10rem;
+    margin: 5rem 0;
     font-family: var(--font-primary);
     font-size: 4rem;
     font-weight: 700;
     line-height: 4.6rem;
+  }
+  h3 {
+    margin: 5rem 0;
+    font-family: var(--font-primary);
+    font-size: 3rem;
+    font-weight: 700;
   }
   .subtitle {
     background: var(--clr-orange-1);
